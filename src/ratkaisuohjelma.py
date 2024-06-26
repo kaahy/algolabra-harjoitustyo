eb6dd5c646fd45915bdafaf3bd27bdfaa2b6f746
kaa <113045348+kaahy@users.noreply.github.com>
2024-06-26 18:46:36 +0300

    Koodin kommentointia ja selventämistä

--- src/ratkaisuohjelma.py
+++ src/ratkaisuohjelma.py
@@ -1,5 +1,10 @@
 class Ratkaisuohjelma():
+    """15-pelin ratkaistavuuden ja lyhyimmän ratkaisun selvittämisestä vastaava luokka."""
+
     def __init__(self, aloitustilanne):
+        """Args:
+            aloitustilanne (tuple): esim. ('1', '10', '6', '4', '5', '2', '15', '3', '9', '7', '12', '8', '13', '-', '14', '11')
+        """
         self.aloitustilanne = aloitustilanne
 
         self.vaiheet = []
@@ -9,6 +14,7 @@ class Ratkaisuohjelma():
         self.vierailulaskuri = 0
 
     def ratkaisu(self):
+        """Palauttaa siirtojärjestyksen listana numeroita, jos ratkaisu löytyy, muutoin None."""
         if not self.onko_ratkaistavissa(self.aloitustilanne):
             return None
         if self.onko_ratkaisu(self.aloitustilanne):
@@ -22,9 +28,11 @@ class Ratkaisuohjelma():
         return self.siirtojarjestys
 
     def valivaiheet(self):
+        """Palauttaa välivaiheet listana tupleja, ml. aloitus- ja ratkaisutilanteet."""
         return self.vaiheet
 
     def paivita_siirtojarjestys(self, vaiheet):
+        """Asettaa self.siirtojarjestys-muuttujaan välivaiheista lasketun siirtojärjestyksen listana numeroita."""
         if not self.ratkaistu:
             return
 
@@ -42,9 +50,10 @@ class Ratkaisuohjelma():
     def onko_ratkaisu(self, tilanne):
         return tilanne == ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '-')
 
-    def onko_ratkaistavissa(self, tilanne):
-        inversioiden_maara = self.laske_inversiot(tilanne)
-        tyhjan_rivinro = tilanne.index('-') // 4
+    def onko_ratkaistavissa(self, aloitustilanne):
+        """Palauttaa tiedon (True/False), onko pelille olemassa ratkaisua."""
+        inversioiden_maara = self.laske_inversiot(aloitustilanne)
+        tyhjan_rivinro = aloitustilanne.index('-') // 4
 
         return (inversioiden_maara + 1+tyhjan_rivinro) % 2 == 0
 
@@ -61,9 +70,9 @@ class Ratkaisuohjelma():
         return inversiot_lkm
 
     def naapuritilanteet(self, tilanne_):
-        # millaiset laattajärjestykset mahdollisia seuraavan siirron jälkeen
-        # palauttaa listan tupleja (etaisyysarvio, naapuri) etäisyysarvion mukaan järjestettynä
-
+        """Selvittää millaiset laattajärjestykset ovat mahdollisia seuraavan siirron jälkeen.
+        Palauttaa listan tupleja (etaisyysarvio, naapuri) etäisyysarvion mukaan järjestettynä.
+        """
         tilanne = list(tilanne_)
         vaihtoehdot = []
 
@@ -73,30 +82,29 @@ class Ratkaisuohjelma():
         if i not in [3, 7, 11, 15]:
             tilanne2 = tilanne.copy()
             tilanne2[i], tilanne2[i+1] = tilanne[i+1], '-'
-            vaihtoehdot.append((self.laske_kustannus(tilanne2), tuple(tilanne2)))
+            vaihtoehdot.append((self.arvio_tulevista_siirroista(tilanne2), tuple(tilanne2)))
         # siirto vasemmalta
         if i not in [0, 4, 8, 12]:
             tilanne2 = tilanne.copy()
             tilanne2[i], tilanne2[i-1] = tilanne[i-1], '-'
-            vaihtoehdot.append((self.laske_kustannus(tilanne2), tuple(tilanne2)))
+            vaihtoehdot.append((self.arvio_tulevista_siirroista(tilanne2), tuple(tilanne2)))
         # siirto ylhäältä
         if i not in [0, 1, 2, 3]:
             tilanne2 = tilanne.copy()
             tilanne2[i], tilanne2[i-4] = tilanne[i-4], '-'
-            vaihtoehdot.append((self.laske_kustannus(tilanne2), tuple(tilanne2)))
+            vaihtoehdot.append((self.arvio_tulevista_siirroista(tilanne2), tuple(tilanne2)))
         # siirto alhaalta
         if i not in [12, 13, 14, 15]:
             tilanne2 = tilanne.copy()
             tilanne2[i], tilanne2[i+4] = tilanne[i+4], '-'
-            vaihtoehdot.append((self.laske_kustannus(tilanne2), tuple(tilanne2)))
+            vaihtoehdot.append((self.arvio_tulevista_siirroista(tilanne2), tuple(tilanne2)))
 
         vaihtoehdot.sort()
 
         return vaihtoehdot
 
     def manhattan_distance(self, tilanne):
-        #summa laattojen etäisyyksistä oikeilta paikoiltaan
-
+        """Palauttaa summan (int) laattojen etäisyyksistä oikeilta paikoiltaan."""
         etaisyyksien_summa = 0
 
         for i in range(16):
@@ -119,23 +127,20 @@ class Ratkaisuohjelma():
 
         return etaisyyksien_summa
 
-    def laske_kustannus(self, tilanne):
-        # heuristinen etäisyysarvio (jäljellä olevat siirrot)
-        # tilapäisesti yksinkertainen
-
+    def arvio_tulevista_siirroista(self, tilanne):
+        """Heuristinen arvio (int) sille, mikä tulevien siirtojen pienin määrä enintään olisi."""
         return self.manhattan_distance(tilanne)
 
-    def ida_syvyyshaku(self, solmu, reitti, kustannusarvio, siirtoja=0):
+    def ida_syvyyshaku(self, tilanne, reitti, arvio_tulevista_siirroista, siirtoja=0):
+        """Solmuissa vierailua. Yksi pelitilanne vastaa yhtä solmua."""
         self.vierailulaskuri += 1
 
-        fscore = siirtoja + kustannusarvio
-
         reitti2 = reitti.copy()
-        reitti2.append(solmu)
+        reitti2.append(tilanne)
 
         if self.ratkaistu:
             return
-        if self.onko_ratkaisu(solmu):
+        if self.onko_ratkaisu(tilanne):
             self.vaiheet = reitti2
             self.ratkaistu = True
             return
@@ -144,22 +149,27 @@ class Ratkaisuohjelma():
         if self.laskuri < 0:
             return
 
+        # jos vähimmäissiirtojen kokonaismääräarvio (fscore) on tälle tilanteelle/solmulle liian suuri (> treshold), solmua ei tutkita enempää nykyisessä iteraatiossa
+        fscore = siirtoja + arvio_tulevista_siirroista
         if fscore > self.iteraation_treshold:
+            # seuraavan iteraation treshold tulee olemaan pienin fscore niiltä solmuilta, joiden fscore nykyisessä iteraatiossa ylittää tresholdin
             self.seuraavan_iteraation_treshold = min(self.seuraavan_iteraation_treshold, fscore)
             return
 
-        for naapuri in self.naapuritilanteet(solmu):
+        for naapuri in self.naapuritilanteet(tilanne):
             if naapuri[1] not in reitti:
                 self.ida_syvyyshaku(naapuri[1], reitti2, naapuri[0], siirtoja+1)
 
     def ida_algoritmi(self, aloitustilanne):
-        aloitustilanteen_etaisyysarvio = self.laske_kustannus(aloitustilanne)
-        self.iteraation_treshold = aloitustilanteen_etaisyysarvio
+        """Etsii ratkaisua ja sen löytyessä päivittää konstruktorin ratkaisumuuttujat."""
+        aloitustilanteen_etaisyysarvio = self.arvio_tulevista_siirroista(aloitustilanne)
+        self.iteraation_treshold = aloitustilanteen_etaisyysarvio # fscoren ylärajoitus, kasvaa iteraatioittain
         self.laskuri = 50000000 # ettei etsitä liian kauan, jos ratkaisua ei löydy
 
         # iteraatioita kunnes ratkaisu löytynyt, aina alkusolmusta aloittaen
+        # joka iteraatiolla edetään solmuissa (pelitilanteissa) syvemmälle kuin edellisellä
         while True:
-            self.seuraavan_iteraation_treshold = 100000 # päivittyy tämän iteraation aikana
+            self.seuraavan_iteraation_treshold = 100000
 
             self.ida_syvyyshaku(aloitustilanne, [], aloitustilanteen_etaisyysarvio)
 
